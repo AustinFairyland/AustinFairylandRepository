8a7a3f2c412a2ac72c2945d21631d709db3d7b6c
Austin <fairylandhost@outlook.com>
2024-01-07 22:44:32 +0800

    :sparkles: Init Commit

---
+++ test.py
@@ -0,0 +1,25 @@
+# coding: utf8
+""" 
+@File: test.py
+@Editor: PyCharm
+@Author: Austin (From Chengdu.China) https://fairy.host
+@HomePage: https://github.com/AustinFairyland
+@OperatingSystem: Windows 11 Professional Workstation 23H2 Canary Channel
+@CreatedTime: 2024-01-07
+"""
+from __future__ import annotations
+
+import sys
+import warnings
+import platform
+import asyncio
+
+sys.dont_write_bytecode = True
+warnings.filterwarnings('ignore')
+if platform.system() == 'Windows':
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+import setuptools
+
+if __name__ == '__main__':
+    print(setuptools.find_packages())
