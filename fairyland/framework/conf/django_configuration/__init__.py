0ab6457c8e084c2fc71eb9493b23939e2b79403a
AustinEngineer <austinfairyland@yeah.net>
2024-01-29 13:13:47 +0800

    :fire: Fixed Refactoring

--- fairyland/framework/conf/django_configuration/__init__.py
+++ fairyland/framework/conf/django_configuration/__init__.py
@@ -5,7 +5,7 @@
 @Author: Austin (From Chengdu.China) https://fairy.host
 @HomePage: https://github.com/AustinFairyland
 @OperatingSystem: Windows 11 Professional Workstation 23H2 Canary Channel
-@CreatedTime: 2023-10-12
+@CreatedTime: 2024-01-29
 """
 from __future__ import annotations
 
@@ -16,15 +16,15 @@ import platform
 import asyncio
 
 sys.dont_write_bytecode = True
-warnings.filterwarnings('ignore')
-if platform.system() == 'Windows':
+warnings.filterwarnings("ignore")
+if platform.system() == "Windows":
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-import time
-import random
+import typing
+import types
 
-from .redis import RedisStandaloneToolsClass
+from .public_configuration import DjangoPublicConfiguration
 
-__all__ = [
-    'RedisStandaloneToolsClass'
+__all__: list = [
+    "DjangoPublicConfiguration",
 ]
