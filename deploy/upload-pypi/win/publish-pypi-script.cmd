b98770eb1183639bbd1e80ed1990f07acdd28bac
Lionel Johnson's PC for work <austinfairyland@yeah.net>
2024-02-29 18:41:55 +0800

    :bug: fixed a bug

--- deploy/upload-pypi/win/publish-pypi-script.cmd
+++ deploy/upload-pypi/win/publish-pypi-script.cmd
@@ -3,4 +3,5 @@ pip install --upgrade setuptools wheel -i https://pypi.tuna.tsinghua.edu.cn/simp
 python setup.py sdist bdist_wheel
 pip install twine -i https://pypi.tuna.tsinghua.edu.cn/simple
 pip install --upgrade twine -i https://pypi.tuna.tsinghua.edu.cn/simple
+python setup.py sdist bdist_wheel
 twine upload dist/*
